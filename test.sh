d6ec8ed9bc0d7c161ed1f8f551d29a61d4eca9cf
Huỳnh Đình Vũ <44773042+boiprcvn3@users.noreply.github.com>
2022-01-03 20:42:33 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pid=`ps aux | grep -e "ssh.*.p22" -m1 | awk '{print $2}'`
+echo $pid
 host=`ps aux | grep -e "ssh.*.p22" -m1 | awk '{print $11" "$12" "$13}'`
 sudo strace -e trace=read -p $pid 2>&1 | while read -r a; do grep '^read.*= [1-9]$' <<<"$a";done | cut -d "\"" -f 2 > logger.txt
 
